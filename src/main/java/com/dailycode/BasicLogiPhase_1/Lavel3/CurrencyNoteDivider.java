31ea5df7745711a49f6429409753f6c01880a58a
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-09 09:58:40 +0530

    added the code

--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel3/CurrencyNoteDivider.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel3/CurrencyNoteDivider.java
@@ -2,7 +2,12 @@ package com.dailycode.BasicLogiPhase_1.Lavel3;
 
 public class CurrencyNoteDivider {
     public static void main(String[] args) {
-
+        int num=10000;
+        if(num>0 && num%100==0 && num%500==0 && num%2000==0){
+            System.out.println("note can be divided");
+        }else {
+            System.out.println("not cab not be breakable");
+        }
     }
 }
 
