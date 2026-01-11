a376cf91b5b7ab08e2633498b092a8463f8c22b1
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-11 10:03:11 +0530

    added the code

--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/PythagoreanTripletChecker.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/PythagoreanTripletChecker.java
@@ -1,8 +1,19 @@
 package com.dailycode.BasicLogiPhase_1.Lavel5;
 
+import java.util.Scanner;
+
 public class PythagoreanTripletChecker {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int c=in.nextInt();
 
+        if( (a*a+b*b==c*c) || (b*b+c*c==a*a) || (c*c+a*a==b*b)){
+            System.out.println("its a pythagorean triplet");
+        }else {
+            System.out.println("its not a pythagorean triplet");
+        }
     }
 }
 
