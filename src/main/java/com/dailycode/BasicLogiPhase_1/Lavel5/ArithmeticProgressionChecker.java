f2f02e4caefe641fddae091afeebde2b22869a11
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-11 10:14:22 +0530

    added the code

--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/ArithmeticProgressionChecker.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/ArithmeticProgressionChecker.java
@@ -1,8 +1,18 @@
 package com.dailycode.BasicLogiPhase_1.Lavel5;
 
+import java.util.Scanner;
+
 public class ArithmeticProgressionChecker {
     public static void main(String[] args) {
-
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int c=in.nextInt();
+        if((b-a == c-b)){
+            System.out.println("Ap");
+        }else {
+            System.out.println("Not Ap");
+        }
     }
 }
 
--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/GeometricProgressionChecker.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/GeometricProgressionChecker.java
@@ -1,8 +1,19 @@
 package com.dailycode.BasicLogiPhase_1.Lavel5;
 
+import java.util.Scanner;
+
 public class GeometricProgressionChecker {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int c=in.nextInt();
+        if(b*b ==a*c){
+            System.out.println("GP");
+        }else{
+            System.out.println("Not A GP");
 
+        }
     }
 }
 
