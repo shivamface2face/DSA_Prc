905f77a6b64eaad496bee25c20009cf5192117c4
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-11 10:35:44 +0530

    added the code

--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/CalendarDateValidator.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/CalendarDateValidator.java
@@ -1,8 +1,34 @@
 package com.dailycode.BasicLogiPhase_1.Lavel5;
 
+import java.util.Scanner;
+
 public class CalendarDateValidator {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int day=in.nextInt();
+        int month=in.nextInt();
+        boolean isValidDate=false;
+        if(month <1 || month>12 || day<1){
+            isValidDate=false;
+        }else if (month==2){
+            if(day<=28){
+                isValidDate=true;
+            }
+        }else if (month==4 || month==6 || month==9 || month==11){
+            if(day<=30){
+                isValidDate=true;
+            }
+        }else {
+            if (day<=31){
+                isValidDate=true;
+            }
+        }
 
+        if(isValidDate==true){
+            System.out.println("valid date");
+        }else {
+            System.out.println("invalid date");
+        }
     }
 }
 
--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/DigitSumEqualityChecker.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/DigitSumEqualityChecker.java
@@ -1,8 +1,22 @@
 package com.dailycode.BasicLogiPhase_1.Lavel5;
 
+import java.util.Scanner;
+
 public class DigitSumEqualityChecker {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int a=Math.abs(n);
+        int lastDigit=a%10;
+        int mid=(a/10)%10;
+        int first=a/100;
+        if(mid==lastDigit+first){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
 
     }
 }
+
 
