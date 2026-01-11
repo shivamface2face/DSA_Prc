b30b07d5ad59d968aa5c1131fde9d79b03d2db46
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-11 09:58:29 +0530

    added the code

--- src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/AxisOriginChecker.java
+++ src/main/java/com/dailycode/BasicLogiPhase_1/Lavel5/AxisOriginChecker.java
@@ -1,8 +1,19 @@
 package com.dailycode.BasicLogiPhase_1.Lavel5;
 
+import java.util.Scanner;
+
 public class AxisOriginChecker {
     public static void main(String[] args) {
-
+        Scanner in=new Scanner(System.in);
+        int x=in.nextInt();
+        int y=in.nextInt();
+        if(x>0 && y==0 || x<0 && y==0){
+            System.out.println("X axis");
+        }else if(y>0 && x==0 || y<0 && x==0){
+            System.out.println("Y axis");
+        }else {
+            System.out.println("origin");
+        }
     }
 }
 
