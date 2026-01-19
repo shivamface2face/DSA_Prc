0067fe9312587c3bb104f418a69278408f4926a9
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-19 10:57:12 +0530

    added the  Arrays code

--- src/main/java/com/dailycode/BasicLogicPhase_3/Lavel3/FirstLastSwapper.java
+++ src/main/java/com/dailycode/BasicLogicPhase_3/Lavel3/FirstLastSwapper.java
@@ -1,7 +1,13 @@
 package com.dailycode.BasicLogicPhase_3.Lavel3;
 
+import java.util.Arrays;
+
 public class FirstLastSwapper {
     public static void main(String[] args) {
         int[] arr = {12, -5, 8, -3, 7, 4, -9, 6, 15, 2};
+        int temp=arr[0];
+        arr[0]=arr[arr.length-1];
+        arr[arr.length-1]=temp;
+        System.out.println(Arrays.toString(arr));
     }
 }
