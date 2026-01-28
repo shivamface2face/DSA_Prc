1edfdfd4599ea2ce323d1a6234ffd5e9c9a24b95
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-28 09:35:00 +0530

    added the  String  code

--- src/main/java/com/dailycode/BasicLogicPhase_4/Lavel2/AlphabetBeforeAfterM.java
+++ src/main/java/com/dailycode/BasicLogicPhase_4/Lavel2/AlphabetBeforeAfterM.java
@@ -2,6 +2,29 @@ package com.dailycode.BasicLogicPhase_4.Lavel2;
 
 public class AlphabetBeforeAfterM {
     public static void main(String[] args) {
+    String string = "Hellow";
+     int beforeM=0;
+     int afterM=0;
+//     for(int i=0;i<string.length();i++){
+//         char ch=string.charAt(i);
+//         if((ch>='A' && ch<='M') || (ch>='a' && ch<='m')){
+//             beforeM++;
+//         }else {
+//             afterM++;
+//         }
+//     }
 
+//        another way using Character.toLowerCase()
+        for(char ch:string.toCharArray()){
+            char toUpper=Character.toUpperCase(ch);
+            if(toUpper<='M'){
+                beforeM++;
+            }else {
+                afterM++;
+            }
+        }
+
+        System.out.println("Alphabets before M :"+beforeM);
+        System.out.println("Alphabets after M :"+afterM);
     }
 }
