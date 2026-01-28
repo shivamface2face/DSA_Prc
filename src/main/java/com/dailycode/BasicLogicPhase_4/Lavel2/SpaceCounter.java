923b275a4e57aa4fb5bbfd7e708c80e8983d4e9c
Shivam Singh <shivamfaceuit@gmail.com>
2026-01-28 09:39:10 +0530

    added the  String  code

--- src/main/java/com/dailycode/BasicLogicPhase_4/Lavel2/SpaceCounter.java
+++ src/main/java/com/dailycode/BasicLogicPhase_4/Lavel2/SpaceCounter.java
@@ -2,6 +2,18 @@ package com.dailycode.BasicLogicPhase_4.Lavel2;
 
 public class SpaceCounter {
     public static void main(String[] args) {
-
+    String str="Count the number of spaces in the given string";
+    int count=0;
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i)==' '){
+//                 count++;
+//             }
+//         }
+        for(char ch:str.toCharArray()){
+            if (ch==' '){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
